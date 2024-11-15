800ddfb6fde4df770d28913eb404c790a7e1a676
unknown <kyawzinhein162@gmail.com>
2024-11-15 11:42:16 +0630

    crud done

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,137 @@
-import Image from "next/image";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default async function Home() {
+  async function createPost(formData: FormData): Promise<void> {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const body = formData.get("body") as string;
+
+    const post = await db.post.create({
+      data: {
+        title: title,
+        body: body,
+      },
+    });
+    revalidatePath("/");
+    // return post;
+  }
+
+  const postData = await db.post.findMany();
+
+  async function updatePost(formData: FormData): Promise<void> {
+    "use server";
+
+    const id = formData.get("id") as string;
+    const title = formData.get("title") as string;
+    const body = formData.get("body") as string;
+
+    const post = await db.post.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        body: body,
+      },
+    });
+    revalidatePath("/");
+    // return post;
+  }
+
+  async function deletePost(formData: FormData): Promise<void> {
+    "use server";
+
+    const id = formData.get("id") as string;
+
+    const post = await db.post.delete({
+      where: {
+        id: id,
+      },
+    });
+    revalidatePath("/");
+    // return post;
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="max-w-xl mx-auto">
+      <Card className="mb-10">
+        <CardHeader>
+          <CardTitle>CRUD App</CardTitle>
+          <CardDescription>Create your love...</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={createPost}>
+            <label htmlFor="title">Title</label>
+            <Input
+              type="text"
+              name="title"
+              className="mb-3"
+              placeholder="title"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <label htmlFor="body">Body</label>
+            <Input type="body" name="body" placeholder="body" />
+            <Button className="w-full mt-4">Save</Button>
+          </form>
+        </CardContent>
+      </Card>
+
+      <div>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          All Posts
+        </h2>
+        {postData.map((post) => (
+          <div className="mb-8 shadow-lg">
+            <Card>
+              <form action={updatePost}>
+                <CardHeader>
+                  <CardTitle>{post.title}</CardTitle>
+                  <CardDescription>{post.body}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Input name="id" defaultValue={post.id} type="hidden" />
+                  <label htmlFor="title">Title</label>
+                  <Input
+                    type="text"
+                    name="title"
+                    className="mb-3"
+                    placeholder="title"
+                    defaultValue={post.title}
+                  />
+                  <label htmlFor="body">Body</label>
+                  <Input
+                    type="body"
+                    name="body"
+                    placeholder="body"
+                    defaultValue={post.body}
+                  />
+                </CardContent>
+                <CardFooter>
+                  <Button className="w-full mt-4">Update</Button>
+                </CardFooter>
+              </form>
+              <CardContent>
+                <form action={deletePost}>
+                  <Input name="id" defaultValue={post.id} type="hidden" />
+                  <Button variant="destructive">Delete</Button>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
